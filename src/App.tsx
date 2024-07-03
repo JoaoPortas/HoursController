552cdcd7ea708b6537afc83bb17aaf413e258497
João Portas <58847955+JoaoPortas@users.noreply.github.com>
2024-07-04 00:32:54 +0100

    Basic Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
-const elm = document.getElementById('app');
-if (elm !== null) {
-  const root = createRoot(elm);
-  root.render(<h1>Hello, world</h1>);
-}
-
+const root = createRoot(document.body);
+root.render(<h2>Hello from React!</h2>);
