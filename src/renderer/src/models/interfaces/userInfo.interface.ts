3c969569861d86e7909c58efcc4d6b7fd7568e99
João Portas <58847955+JoaoPortas@users.noreply.github.com>
2024-07-06 13:00:17 +0100

    User models and signup setup

---
+++ src/renderer/src/models/interfaces/userInfo.interface.ts
@@ -0,0 +1,6 @@
+export interface IBaseUserInfo {
+    number: string,
+    name: string,
+    category: string,
+    position: string
+}
--- src/renderer/src/views/auth/Signup.tsx
+++ src/renderer/src/views/auth/Signup.tsx
@@ -1,12 +1,32 @@
-import React, { useEffect } from "react";
+import React, { FormEvent, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "@renderer/public/css/Login.module.css"
+import { UserRegist } from "@renderer/models/auth.model";
+//import { setupUsers } from "@renderer/repository/setup.repository";
 
 const Signup: React.FC = () => {
+
+    function createUser(event: FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        const form: HTMLFormElement = event.target as HTMLFormElement;
+
+        const name:string = (form.elements.namedItem("name") as HTMLInputElement).value
+        const username:string = (form.elements.namedItem("username") as HTMLInputElement).value
+        const password:string = (form.elements.namedItem("password") as HTMLInputElement).value
+        const personalId:string = (form.elements.namedItem("number") as HTMLInputElement).value
+        const category:string = (form.elements.namedItem("category") as HTMLInputElement).value
+        const workerFunction:string = (form.elements.namedItem("function") as HTMLInputElement).value
+
+        const newUser: UserRegist = new UserRegist(username, password, personalId, name, category, workerFunction)
+        //setupUsers();
+        //createUser(UserRegist)
+    }
+
     useEffect(() => {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         const forms:NodeListOf<HTMLFormElement> = document.querySelectorAll('.needs-validation');
+        //let signupForm: HTMLFormElement = document.getElementById('signupForm') as HTMLFormElement ?? null;
 
         // Loop over them and prevent submission
         Array.from(forms).forEach(form => {
@@ -19,19 +39,24 @@ const Signup: React.FC = () => {
                 form.classList.add('was-validated');
             }, false);
         });
+
+        /*signupForm.addEventListener('submit', (event: Event) => {
+            if ()
+        });*/
+
     }, []);
 
     return (
         <main>
             <div style={{margin: "50px auto auto auto", width: "60%", minWidth: "300px", maxWidth: "700px"}}>
-                <form className="needs-validation" noValidate>
+                <form id="signupForm" className="needs-validation" onSubmit={createUser} noValidate>
                     <div className="mb-3" style={{textAlign: "center"}}>
                         <h1 style={{marginBottom: "30px"}}>Novo utilizador</h1>
                     </div>
                     <div className="row mb-3">
                         <label htmlFor="name" className="col-sm-4 col-form-label">Nome*</label>
                         <div className="col-sm-8">
-                            <input type="text" className="form-control" id="name" required/>
+                            <input type="text" className="form-control" id="name" name="name" required/>
                             <div className="invalid-feedback">
                                 *Campo de preenchimento obrigatório
                             </div>
@@ -40,7 +65,7 @@ const Signup: React.FC = () => {
                     <div className="row mb-3">
                         <label htmlFor="username" className="col-sm-4 col-form-label">Nome de utilizador*</label>
                         <div className="col-sm-8">
-                            <input className="form-control" id="username" required/>
+                            <input className="form-control" id="username" name="username" required/>
                             <div className="invalid-feedback">
                                 *Campo de preenchimento obrigatório
                             </div>
@@ -49,7 +74,7 @@ const Signup: React.FC = () => {
                     <div className="row mb-3">
                         <label htmlFor="password" className="col-sm-4 col-form-label">Código de acesso*</label>
                         <div className="col-sm-8">
-                            <input type="password" className="form-control" id="password" required/>
+                            <input type="password" className="form-control" id="password" name="password" required/>
                             <div className="invalid-feedback">
                                 *Campo de preenchimento obrigatório
                             </div>
@@ -58,7 +83,7 @@ const Signup: React.FC = () => {
                     <div className="row mb-3">
                         <label htmlFor="number" className="col-sm-4 col-form-label">NIP/MÓD.*</label>
                         <div className="col-sm-8">
-                            <input className="form-control" id="number" required/>
+                            <input className="form-control" id="number" name="number" required/>
                             <div className="invalid-feedback">
                                 *Campo de preenchimento obrigatório
                             </div>
@@ -67,7 +92,7 @@ const Signup: React.FC = () => {
                     <div className="row mb-3">
                         <label htmlFor="category" className="col-sm-4 col-form-label">Categoria/Carreira*</label>
                         <div className="col-sm-8">
-                            <input className="form-control" id="category" required/>
+                            <input className="form-control" id="category" name="category" required/>
                             <div className="invalid-feedback">
                                 *Campo de preenchimento obrigatório
                             </div>
@@ -76,7 +101,7 @@ const Signup: React.FC = () => {
                     <div className="row mb-3">
                         <label htmlFor="function" className="col-sm-4 col-form-label">Função*</label>
                         <div className="col-sm-8">
-                            <input className="form-control" id="function" required/>
+                            <input className="form-control" id="function" name="function" required/>
                             <div className="invalid-feedback">
                                 *Campo de preenchimento obrigatório
                             </div>
