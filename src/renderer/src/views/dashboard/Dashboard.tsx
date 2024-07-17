46efe1da30aad4b147d660ac7beddc5c0380e90f
João Portas <58847955+JoaoPortas@users.noreply.github.com>
2024-07-17 23:52:45 +0100

    Circular progress bar

--- src/renderer/src/views/dashboard/Dashboard.tsx
+++ src/renderer/src/views/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { RootState } from "@renderer/redux/store";
 import { IExtraHoursResume } from "@shared/models/hours/interfaces/extraHoursResume.interface";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -15,6 +15,30 @@ const Dashboard: React.FC = () => {
         console.log(data)
     }
 
+    useEffect(() => {
+        document.querySelectorAll('.progress').forEach((element) => {
+          const value = element.getAttribute('data-value');
+          if (value) {
+            const progressValue = parseInt(value, 10);
+            const left = element.querySelector('.progress-left .progress-bar') as HTMLElement;
+            const right = element.querySelector('.progress-right .progress-bar') as HTMLElement;
+
+            if (progressValue > 0) {
+              if (progressValue <= 50) {
+                right.style.transform = `rotate(${percentageToDegrees(progressValue)}deg)`;
+              } else {
+                right.style.transform = `rotate(180deg)`;
+                left.style.transform = `rotate(${percentageToDegrees(progressValue - 50)}deg)`;
+              }
+            }
+          }
+        });
+      }, []);
+
+      const percentageToDegrees = (percentage: number): number => {
+        return (percentage / 100) * 360;
+      };
+
     test()
 
     return (
@@ -22,6 +46,34 @@ const Dashboard: React.FC = () => {
             <main>
                 <h1>Dashboard</h1>
                 {userId && <p>Logged in as user ID: {userId}</p>}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-3 col-lg-6 mb-4">
+                            <div className="bg-white rounded-lg p-5 shadow">
+                                <h2 className="h6 font-weight-bold text-center mb-4">Overall progress</h2>
+                                <div className="progress mx-auto" data-value='70'>
+                                <span className="progress-left">
+                                    <span className="progress-bar border-primary"></span>
+                                </span>
+                                <span className="progress-right">
+                                    <span className="progress-bar border-primary"></span>
+                                </span>
+                                <div className="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                                    <div className="h2 font-weight-bold">80<sup className="small">%</sup></div>
+                                </div>
+                                </div>
+                                <div className="row text-center mt-4">
+                                <div className="col-6 border-right">
+                                    <div className="h4 font-weight-bold mb-0">28%</div><span className="small text-gray">Last week</span>
+                                </div>
+                                <div className="col-6">
+                                    <div className="h4 font-weight-bold mb-0">60%</div><span className="small text-gray">Last month</span>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Link to="/">Go to /</Link>
             </main>
         </>
