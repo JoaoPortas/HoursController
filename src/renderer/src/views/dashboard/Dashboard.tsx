efd1bf53c01ad57245c21eda98db4abcc052485f
João Portas <58847955+JoaoPortas@users.noreply.github.com>
2024-07-18 23:15:16 +0100

    Test loading circle bar

--- src/renderer/src/views/dashboard/Dashboard.tsx
+++ src/renderer/src/views/dashboard/Dashboard.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
     const userId = useSelector((state: RootState) => state.userSession.userId)
+    const [totalHours, setTotalHours] = useState(0)
     const [currentMonth, _setCurrentMonth] = useState<string>(
         (new Date().getMonth() + 1).toString().padStart(2, '0')
     );
@@ -13,6 +14,14 @@ const Dashboard: React.FC = () => {
     async function test() {
         const data: IExtraHoursResume[] | null = await window.electron.ipcRenderer.invoke("/hoursManagement/getUserAllExtraHoursResumeByYearAndMonth", userId, new Date().getFullYear().toString(), currentMonth) as IExtraHoursResume[];
         console.log(data)
+
+        let total: number = 0
+
+        data.forEach((elm: IExtraHoursResume) => {
+            total += elm.extraHours
+        })
+        console.log(total)
+        setTotalHours(total)
     }
 
     useEffect(() => {
@@ -46,7 +55,46 @@ const Dashboard: React.FC = () => {
             <main>
                 <h1>Dashboard</h1>
                 {userId && <p>Logged in as user ID: {userId}</p>}
-                <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="card">
+                        <div className="card-body">
+                        <div className="bg-white rounded-lg p-5 shadow">
+                    <div className="row">
+                        <div className="col">
+                            <h6>Horas feitas no mês de Julho: </h6> {totalHours}
+                        </div>
+                        <div className="col">
+                            <div className="progress mx-auto" data-value='70'>
+                                <span className="progress-left">
+                                    <span className="progress-bar border-primary"></span>
+                                </span>
+                                <span className="progress-right">
+                                    <span className="progress-bar border-primary"></span>
+                                </span>
+                                <div className="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                                    <div className="h2 font-weight-bold">80<sup className="small">%</sup></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br />
+                {/*<div className="container">
                     <div className="row">
                         <div className="col-xl-3 col-lg-6 mb-4">
                             <div className="bg-white rounded-lg p-5 shadow">
@@ -73,7 +121,7 @@ const Dashboard: React.FC = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>*/}
                 <Link to="/">Go to /</Link>
             </main>
         </>
