f1309bdd81dc8a21cd35ea1c931095d99084df51
João Portas <58847955+JoaoPortas@users.noreply.github.com>
2024-07-21 16:21:40 +0100

    Hours per tier display on dashboard

--- src/renderer/src/views/dashboard/Dashboard.tsx
+++ src/renderer/src/views/dashboard/Dashboard.tsx
@@ -7,51 +7,94 @@ import { Link } from "react-router-dom";
 const Dashboard: React.FC = () => {
     const userId = useSelector((state: RootState) => state.userSession.userId)
     const [totalHours, setTotalHours] = useState(0)
-    const [currentMonth, _setCurrentMonth] = useState<string>(
+    /*const [currentMonth, _setCurrentMonth] = useState<string>(
         (new Date().getMonth() + 1).toString().padStart(2, '0')
-    );
+    );*/
+    const [hoursAt25, setHoursAt25] = useState(0)
+    const [hoursAt37Dot5, setHoursAt37Dot5] = useState(0)
+
+    const [hoursAt50, setHoursAt50] = useState(0)
+    const [hoursAt75, setHoursAt75] = useState(0)
 
     // Function to calculate hours and percentages
-    async function test(hours: IExtraHoursResume[]) {
+    async function calculateExtraHoursTiers(hours: IExtraHoursResume[]) {
         let totalHours = 0;
         let hoursFor25 = 0;
         let hoursFor37Dot5 = 0;
+        let hoursFor50 = 0;
+        let hoursFor75 = 0;
 
         hours.forEach((elm: IExtraHoursResume) => {
-            totalHours += elm.extraHours;
+            if (elm.extraHours <= 0) return;
 
-            if (elm.extraHours > 0) {
+            //Se as horas totais forem menor que zero adiciona a primeira a 25% e vê as restantes para adicionar a 37.5 se continuar <100
+            if (totalHours < 100) {
                 hoursFor25 += 1;
                 elm.extraHours -= 1;
+                totalHours += 1;
+
                 if (elm.extraHours > 0) {
-                    hoursFor37Dot5 += elm.extraHours;
+                    //Guarda as horas totais atuais para uma variavel para validar se somar se o resto passa ou não das 100
+                    let tempTotalHours = totalHours
+                    tempTotalHours += elm.extraHours
+
+                    //Se as horas totais + as restantes não passa dos 100 então adiciona a 37.5
+                    if (tempTotalHours <= 100) {
+                        hoursFor37Dot5 += elm.extraHours
+                    }
+                    else { //Se as horas totais + as restantes passa dos 100 então calcula quantas horas n passa dos 100 e guarda numa varriável
+                        //E calcula as restantes que passam dos 100 e guarda a 50%
+
+                        //Guarda quantas horas que passaram das 100 para adicionar a 75%
+                        let restFor75 = tempTotalHours - 100
+
+                        //Tira as horas que têm que ir para os 75% calculadas anteriormente para obter para a var as horas até as 100 que vão continuar nos 37.5%
+                        let restFor37Dot5 = elm.extraHours - restFor75
+
+                        hoursFor37Dot5 += restFor37Dot5
+                        hoursFor75 += restFor75
+
+                    }
+                    totalHours += elm.extraHours
                 }
+            } //Se as horas totais já forem superiores a 100 então pode se adicionar de acordo com as percentagens 50% para a primeira hora e 75 para as restantes
+            else {
+                hoursFor50 += 1;
+                elm.extraHours -= 1;
+                totalHours += 1;
+
+                hoursFor75 += elm.extraHours
             }
         });
 
         console.log("Total hours: ", totalHours);
         console.log("Hours at 25%: ", hoursFor25);
         console.log("Hours at 37.5%: ", hoursFor37Dot5);
-    }
-
-    // Fetch user hours and calculate total
-    async function getAndSetUserCurrentYearHours() {
-        const data: IExtraHoursResume[] | null = await window.electron.ipcRenderer.invoke("/hoursManagement/getUserAllExtraHoursResumeByYear", userId, new Date().getFullYear().toString()) as IExtraHoursResume[];
-
-        let total: number = 0;
-        if (data) {
-            data.forEach((elm: IExtraHoursResume) => {
-                total += elm.extraHours;
-            });
-        }
-
-        setTotalHours(total);
-        if (data) {
-            test(data);
-        }
+        console.log("Hours at 50%: ", hoursFor50);
+        console.log("Hours at 75%: ", hoursFor75);
+        setHoursAt25(hoursFor25)
+        setHoursAt37Dot5(hoursFor37Dot5)
+        setHoursAt50(hoursFor50)
+        setHoursAt75(hoursFor75)
     }
 
     useEffect(() => {
+        async function getAndSetUserCurrentYearHours() {
+            const data: IExtraHoursResume[] | null = await window.electron.ipcRenderer.invoke(
+                "/hoursManagement/getUserAllExtraHoursResumeByYear",
+                userId,
+                new Date().getFullYear().toString()
+            ) as IExtraHoursResume[];
+
+            let total: number = 0;
+            if (data) {
+                data.forEach((elm: IExtraHoursResume) => {
+                    total += elm.extraHours;
+                });
+                setTotalHours(total);
+                calculateExtraHoursTiers(data);
+            }
+        }
         getAndSetUserCurrentYearHours();
     }, [userId]); // Run only when userId changes
 
@@ -89,11 +132,11 @@ const Dashboard: React.FC = () => {
                 <div className="container text-center">
                     <div className="row g-2">
                         <div className="col-6">
-                            <div className="p-3 gradient-border">
+                            <div style={{height: "194px"}} className="p-3 gradient-border">
                                 <div className="col">
                                     <div style={{display: "inline-flex"}}>Horas extra totais (2024): {totalHours} <div style={{fontSize: "0.875em", color: "#6c757d"}}><span style={{verticalAlign: "middle", marginLeft: "5px"}}>/100 horas</span></div></div>
                                 </div>
-                                <div style={{marginTop: "15px"}} className="col">
+                                <div style={{marginTop: "25px"}} className="col">
                                     <div className="progress mx-auto" data-value="14">
                                         <span className="progress-left">
                                             <span className="progress-bar border-primary"></span>
@@ -111,7 +154,24 @@ const Dashboard: React.FC = () => {
                             </div>
                         </div>
                         <div className="col-6">
-                            <div className="p-3">Custom column padding</div>
+                            <div style={{height: "194px"}} className="p-3 gradient-border">
+                                <div className="row">
+                                    <div className="col">
+                                        <div>Horas por categoria (2024)</div>
+                                    </div>
+                                </div>
+                                <div style={{marginTop: "13px", fontSize: "17px"}} className="row">
+                                    <div className="col">
+                                        <span style={{width: "100px"}} className="badge text-bg-primary">125%: {hoursAt25}</span>
+                                        <br />
+                                        <span style={{width: "100px"}} className="badge text-bg-secondary">137.5%: {hoursAt37Dot5}</span>
+                                        <br />
+                                        <span style={{width: "100px"}} className="badge text-bg-success">150%: {hoursAt50}</span>
+                                        <br />
+                                        <span style={{width: "100px"}} className="badge text-bg-info">175%: {hoursAt75}</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-6">
                             <div className="p-3">Custom column padding</div>
