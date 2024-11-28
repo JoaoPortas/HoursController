f1fc4eff80bbb41d72e29f128a0c5752e8c30314
João Portas <58847955+JoaoPortas@users.noreply.github.com>
2024-11-28 19:59:32 +0000

    info content fix

--- src/renderer/src/views/Disclaimer/Disclaimer.tsx
+++ src/renderer/src/views/Disclaimer/Disclaimer.tsx
@@ -8,7 +8,7 @@ const Disclaimer: React.FC = () => {
                 <p>A plataforma EHours não está associada a qualquer entidade e foi desenvolvida de forma independente.
                     Esta plataforma não possui qualquer fim lucrativo, pelo que não pode ser utilizada para fins comerciais ou geradores de receita.</p>
                 <h4>Privacidade e Armazenamento de Dados</h4>
-                <p>Privacidade e Armazenamento de Dados
+                <p>
                 A plataforma EHours armazena todos os dados localmente, no dispositivo do utilizador, junto à aplicação, não havendo qualquer transmissão de informações para servidores externos.
                 Este modelo assegura que os dados permanecem no controlo exclusivo do utilizador.</p>
             </div>
